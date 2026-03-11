6338758718045d20d3b9b17a5f9189399ae608dc
JuanG1504 <--juancool132@gmail.com>
2026-03-11 11:46:14 -0600

    funcion suma

---
+++ mirepo1/app.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int sumar(int a, int b) 
+{ 
+    return a+b;
+} 
+int main
+() 
+{ 
+    int num1, num2, resultado;
+     printf(Ingrese el primer entero: );
+      scanf(%d, &num1); 
+      printf(Ingrese el segundo entero: ); 
+      scanf(%d; &num2); 
+      printf(la suma es: %dn, resultado); 
+      return 0;
+    }
